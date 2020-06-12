a10badaa005eb76d708e4a024759bf5f0e9c2d33
Lauren Emick <laurenemick6@gmail.com>
2020-06-12 09:11:19 -0700

    uploaded

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+import React from 'react'
+
+export default function Character(props) {
+    const { characters } = props
+
+    return (
+        <div className='character'>{characters.name}</div>
+    )
+}
