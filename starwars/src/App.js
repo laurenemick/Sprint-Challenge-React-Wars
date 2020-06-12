7767c5188ce58bb2f02176b44c271378aadc941f
Lauren Emick <laurenemick6@gmail.com>
2020-06-12 10:44:36 -0700

    added styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
 import './App.css';
 import axios from 'axios'
-
 import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState([])
+  const [charactersArr, setCharactersArr] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -15,7 +15,7 @@ const App = () => {
     axios.get('https://swapi.py4e.com/api/people')
       .then(res => {
         console.log(res.data.results)
-        setCharacters(res.data.results)
+        setCharactersArr(res.data.results)
       })
       .catch(err => {
         debugger
@@ -24,10 +24,19 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      <Character characters={characters} />
+      <StyledHeading className="Header">React Wars</StyledHeading>
+      <div> 
+        <Character characters={charactersArr} />
+      </div>
     </div>
   )
 }
+
+const StyledHeading = styled.h1`
+  color: white;
+  letter-spacing: 2px;
+  font-family: 'Alfa Slab One', cursive;
+  font-size: 2rem;
+`;
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,21 +1,22 @@
 // Write your Character component here
 import React from 'react'
+import DivStyling from './divStyling'
 
 export default function Character(props) {
     const { characters } = props
 
-    const Character = ({ info }) => (
-        <div className='character'>
+    const EachCharacter = ({ info }) => (
+        <DivStyling className='character'>
           <div className='name'>{info.name}</div>
           <div className='birthyear'> {info.birth_year}</div>
-        </div>
+        </DivStyling>
       )
 
     return (
         <div className='characterContainer'>
         {
         characters.map(char => {
-          return <Character key={char.name} info={char} />
+          return <EachCharacter key={char.name} info={char} />
         })
       }
       </div>
